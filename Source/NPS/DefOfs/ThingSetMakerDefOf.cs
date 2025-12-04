1df12c21d41774fa3db9f827ccf0d102991aeec5
Halicade <qstr23@hotmail.com>
2025-12-04 08:47:56 -0500

    Added defofs

---
+++ Source/NPS/DefOfs/ThingSetMakerDefOf.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+
+namespace TKKN_NPS;
+
+[DefOf]
+public class ThingSetMakerDefOf
+{
+    public static ThingSetMakerDef TKKN_TidalLoot;
+
+
+    static ThingSetMakerDefOf() {
+        DefOfHelper.EnsureInitializedInCtor(typeof(ThingSetMakerDefOf));
+    }
+}
